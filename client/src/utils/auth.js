18ae62acf6871087ced18dc0226fa20ab58186f4
Charles Young <ctyoungiv@gmail.com>
2022-08-02 11:22:17 -0400

    added commits to auth and added in a temple for globle state and reducers

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -24,15 +24,18 @@ class AuthService {
   }
 
   getToken() {
+    // Retrieves the user token from localStorage
     return localStorage.getItem('id_token');
   }
 
   login(idToken) {
+    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
 
   logout() {
+    // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
     window.location.reload();
   }
